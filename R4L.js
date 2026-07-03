03e1eef12383eca430d070f5fa755aa8b87abd21
iki1uc <entertrainer@web.de>
2026-07-03 19:24:01 +0200

    Update R4L.js

--- R4L.js
+++ R4L.js
@@ -1,1 +1,16 @@
+export const R4L = {
+    name: "R4L",
+    type: "abgang",
+    echo: true,
+
+    run(pxOut) {
+        return {
+            move: "R4L",
+            action: "abgang",
+            echo: true,
+            role: pxOut.role,
+            text: pxOut.text
+        };
+    }
+};
 
